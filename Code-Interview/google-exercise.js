ef42aac180e282be928d543e28268cb79a686dad
Akiko Green <agreen@alphaworks.tech>
2023-01-06 10:01:11 -0800

    Code-interview: complete section

--- Code-Interview/google-exercise.js
+++ Code-Interview/google-exercise.js
@@ -62,3 +62,38 @@ function containsCommons(arr1,arr2){
 //can we assume always 2 params? 
 
 //naming conventions & i & j unless its for loops (its a standard for indices)
+
+
+//Naive solution
+let array3 = [2,3,4,6]
+let num = 12
+
+function hasPairWithSum(arr,sum){
+  var len = arr.length;
+  for(var i = 0; i < len - 1; i++){
+    for(var j = i+1; j < len; j++){
+      if(arr[i] + arr[j] === sum){
+        return true 
+      }
+    }
+  }
+  return false 
+}
+
+// Better solution
+function hasPairWithSum(arr, sum){
+  const mySet = new Set()
+  
+  const len = arr.length
+  
+  for(let i = 0; i < len; i++){
+    if(mySet.has(arr[i])){
+      return true
+    }
+  }
+  return false
+}
+
+
+
+console.log(hasPairWithSum(array3, num))
